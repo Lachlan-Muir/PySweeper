92d95b999d50c55d87bae8c98ff5519afb53434b
Lachlan-Muir <lachlanmuir06@gmail.com>
2023-03-01 14:18:10 -0600

    Implemented string representation of board, within_bounds, and adjacent_tiles

--- board.py
+++ board.py
@@ -13,12 +13,36 @@ Poke: tests a point by revealing it. If the point had a mine, the player loses. 
 point and all connected empty points are revealed.
 """
 
+
 class Board:
+    """
+    A class representation of a Minesweeper board. Mines are represented by 'M', and empty spaces are represented by
+    '.'. Tiles not yet revealed to the player are represented by 'H' and adjacent mine counts are represented by
+    integers. Flags places by the player are represented by 'F'.
+
+    Attributes:
+        width       The width (x-axis) of the board
+        height      The height (y-axis) of the board
+        h_board     The board hidden from the player
+        v_board     The board visible to the player
+
+    Methods:
+        poke(x, y)  Reveals the tile at the given coordinates to the player
+        flag(x, y)  Places a flag at the given coordinates
+    """
+
     def __init__(self, h_board):
+        """
+        :param h_board: A two-dimensional array of characters representing the board hidden from the player.
+        """
         self.width = len(h_board[0])
         self.height = len(h_board)
         self.h_board = h_board
         self.v_board = [['.' for i in range(self.width)] for j in range(self.height)]
+        self.__MINE_TILE = 'M'
+        self.__EMPTY_TILE = '.'
+        self.__HIDDEN_TILE = 'H'
+        self.__FLAG_TILE = 'F'
 
     def __str__(self):
         """
@@ -41,8 +65,83 @@ class Board:
 
         return output
 
+    def within_bounds(self, x, y):
+        """
+        Checks if the given coordinates are within the bounds of the board.
+        :param x: the x-axis coordinate.
+        :param y: the y-axis coordinate.
+        :return: True if the coordinates are in bounds, False if they are out.
+        """
+        return 0 <= x < self.width and 0 <= y < self.height
+
     def poke(self, x, y):
-        return
+        """
+        Takes x and y coordinates and reveals the tile at that location on the board to the player. Nearby tiles
+        which do not have mines adjacent to them are also revealed. The hidden board is not modified.
+
+        :param x: the x-axis coordinate. Must be greater than 0 and less than the width of the board.
+        :param y: the y-axis coordinate. Must be greater than 0 and less than the height of the board.
+        :return: True if a mine was poked, False otherwise
+        """
+        # make sure we have valid coordinates
+        assert self.within_bounds(x, y)
+
+        poked_char = self.h_board[y][x]
+        
+        if poked_char == self.__MINE_TILE:
+            # update the visible board
+            self.v_board[y][x] = poked_char
+            return True
+        elif poked_char == self.__EMPTY_TILE:
+            # if the tile was empty and has no adjacent mines, then we want to poke the tiles around it as well so that
+            # the player does not have to enter a bunch of unnecessary poke commands
+            if self.__adjacent_mine_count(x, y) == 0:
+                for tile in self.__adjacent_tiles(x, y):
+                    self.poke(tile[0], tile[1])
+
+            return False
+
+
+    def __adjacent_tiles(self, x, y):
+        """
+        Obtains all coordinates adjacent to the given one. If an adjacent tile would be outside the bounds of the board,
+        it is not included in the result.
+        :param x: the x-axis coordinate. Must be greater than 0 and less than the width of the board.
+        :param y: the y-axis coordinate. Must be greater than 0 and less than the height of the board.
+        :return: a list of tuples (x, y), each representing a coordinate pair adjacent to the given coordinates.
+        """
+        # make sure we have valid coordinates
+        assert self.within_bounds(x, y)
+        #               NW,       N,       NE,      E,      SE,     S,      SW,      W
+        translations = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+        adj_tiles = []
+        for t in translations:
+            if self.within_bounds(x + t[0], y + t[1]):
+                adj_tiles.append((x + t[0], y + t[1]))
+
+        return adj_tiles
+
+    def __adjacent_mine_count(self, x, y):
+        """
+        Counts the number of mines adjacent to the given coordinates
+        :param x: the x-axis coordinate. Must be greater than 0 and less than the width of the board.
+        :param y: the y-axis coordinate. Must be greater than 0 and less than the height of the board.
+        :return: an integer count of adjacent mines
+        """
+        # make sure we have valid coordinates
+        assert self.within_bounds(x, y)
+        # TODO
+        count = 0
+        return count
 
     def flag(self, x, y):
+        """
+        Takes x and y coordinates and places a flag at that location on the board visible to the player. The hidden
+        board is not modified. If a tile containing a flag is flagged, it will be reverted to a hidden tile.
+        :param x: the x-axis coordinate
+        :param y: the y-axis coordinate
+        :return: None
+        """
+        # make sure we have valid coordinates
+        assert self.within_bounds(x, y)
         return
--- tests/board_test.py
+++ tests/board_test.py
@@ -4,7 +4,7 @@ from board import Board
 
 class TestBoard(unittest.TestCase):
     def setUp(self):
-        self.board_3x3 = Board([['b', '.', '.'],
+        self.board_3x3 = Board([['M', '.', '.'],
                                 ['.', '.', '.'],
                                 ['.', '.', '.']])
 
@@ -21,6 +21,32 @@ class TestBoard(unittest.TestCase):
                          "0  . . .\n" +
                          "1  . . .\n" +
                          "2  . . .\n", str(self.board_3x3))
+
+    def test_within_bounds(self):
+        # test outer edges of board within range
+        self.assertTrue(self.board_3x3.within_bounds(0, 0))
+        self.assertTrue(self.board_3x3.within_bounds(2, 0))
+        self.assertTrue(self.board_3x3.within_bounds(0, 2))
+        # test outer edges of board outside range
+        self.assertFalse(self.board_3x3.within_bounds(-1, 0))
+        self.assertFalse(self.board_3x3.within_bounds(0, -1))
+        self.assertFalse(self.board_3x3.within_bounds(3, 0))
+        self.assertFalse(self.board_3x3.within_bounds(0, 3))
+
+    def test_adjacent_tiles(self):
+        # corners
+        self.assertEqual([(1, 0), (1, 1), (0, 1)], self.board_3x3._Board__adjacent_tiles(0, 0))
+        self.assertEqual([(2, 1), (1, 1), (1, 0)], self.board_3x3._Board__adjacent_tiles(2, 0))
+        self.assertEqual([(1, 1), (2, 1), (1, 2)], self.board_3x3._Board__adjacent_tiles(2, 2))
+        self.assertEqual([(0, 1), (1, 1), (1, 2)], self.board_3x3._Board__adjacent_tiles(0, 2))
+        # middle
+        self.assertEqual([(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (1, 2), (0, 2), (0, 1)],
+                         self.board_3x3._Board__adjacent_tiles(1, 1))
+        # edges
+        self.assertEqual([(2, 0), (2, 1), (1, 1), (0, 1), (0, 0)], self.board_3x3._Board__adjacent_tiles(1, 0))
+        self.assertEqual([(1, 0), (2, 0), (2, 2), (1, 2), (1, 1)], self.board_3x3._Board__adjacent_tiles(2, 1))
+        self.assertEqual([(0, 1), (1, 1), (2, 1), (2, 2), (0, 2)], self.board_3x3._Board__adjacent_tiles(1, 2))
+        self.assertEqual([(0, 0), (1, 0), (1, 1), (1, 2), (0, 2)], self.board_3x3._Board__adjacent_tiles(0, 1))
 
 
 if __name__ == '__main__':
